ad469c84224beb97e367c3318a86a29c2a97fa2c
João Vitor <83173020+JoaoVitor733@users.noreply.github.com>
2023-04-01 14:47:13 -0300

    Update numSucessorAntecessor.cpp

--- Lista 2/numSucessorAntecessor.cpp
+++ Lista 2/numSucessorAntecessor.cpp
@@ -1,11 +1,11 @@
-/*
+
 #include <iostream>
 using namespace std;
 /*
 Elabore um algoritmo que leia um número inteiro e uma letra (“a” – antecessores ou “s” – sucessores) e 
 mostre a soma dos seus próximos 10 antecessores ou sucessores de acordo com a letra digitada. 
 */
-/*
+
 int main(){
     int v;
     char l;
@@ -18,4 +18,4 @@ int main(){
     if(l == 'a') cout << "Soma dos antecessores: " << 5*((v-1)+(v-10));    
     else if(l == 's') cout << "Soma dos sucessores: " << 5*((v+1)+(v+10));  
           else cout << "\nValor invalido!!";
-}*/
+}

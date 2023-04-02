9b8c82081cab83cc43884e58dc4bf5d933c26d9d
João Vitor <83173020+JoaoVitor733@users.noreply.github.com>
2023-04-02 10:00:42 -0300

    adding new resolutions

---
+++ Lista 1/juros.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+/*
+Leia um número inteiro N e um percentual qualquer e escreva qual o valor do percentual 
+aplicado ao número. 
+*/
+int main(){
+    int n;
+    float percentual;
+
+    cout << "\nDigite um numero inteiro: ";
+    cin >> n;
+    cout << "\nDigite um percentual: ";
+    cin >> percentual;
+
+    cout << "\nO juros eh: " << (percentual/100)*n;
+}
---
+++ Lista 1/parcelaCompras.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+/*
+Leia o valor de uma compra nas Lojas Sua Melhor Compra e mostre o valor das prestações de 
+acordo com a quantidade de parcelas desejada pelo usuário. A loja está vendendo seus produtos 
+a vista ou parcelado em até 10 vezes sem juros. */
+int main(){
+    float valor;
+    int parcelas;
+    cout << "\nDigite o valor da sua compra: ";
+    cin >> valor;
+    cout << "\nParcelamos em ate 10 vezes sem juros, deseja parcelar em qaunatas vezes? ";
+    cin >> parcelas;
+    cout << "\nValor de cada parcela: " << valor/parcelas;
+}

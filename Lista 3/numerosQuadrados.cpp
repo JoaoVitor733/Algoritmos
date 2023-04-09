c7e9b6664f8cadd969ca616461abaa83eb7b2ee0
João Vitor <83173020+joaovitor73@users.noreply.github.com>
2023-04-09 16:41:24 -0300

    Correction in file

--- Lista 3/numerosQuadrados.cpp
+++ Lista 3/numerosQuadrados.cpp
@@ -4,5 +4,21 @@ using namespace std;
 mostrar os próximos 10 quadrangulares depois dele. Se não for, informar. OBS: Os números 
 quadrangulares são 1, 4, 9, 16, 25, ..., ou seja, 1x1, 2x2, 3x3, 4x4, ...*/
 int main(){
-    short int number;
-}
+    short int value;
+    int number;
+    bool flag = true;
+    do{ cout << "\nEnter a number: ";
+        cin >> number;
+    }while(number <= 0);
+    for(int i = 1; i < number; i++){
+        if(number == i*i){
+            flag = false;
+            value = i;
+            break;
+        }
+    }
+    if(flag) cout << "\nIt's not a perfect square!";
+    else for(int i = value+1; i < value+10; i++){
+            cout << " " << i*i;
+         }
+} 

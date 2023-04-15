ddfbc9e5e546d3963c9518255bd6ef9b0db4c540
João Vitor <83173020+joaovitor73@users.noreply.github.com>
2023-04-15 20:26:40 -0300

    Update fatorial.cpp

--- Lista 3/fatorial.cpp
+++ Lista 3/fatorial.cpp
@@ -1,10 +1,10 @@
- #include <iostream>
+#include <iostream>
  using namespace std;
  /*
  Elabore um algoritmo que leia um número inteiro e escreva o seu fatorial. Ex.: Fatorial de 4 = 4! = 4 X 3 X 
 2 X 1 = 24. Após o fatorial, verifique e escreva o próximo número divisível pelo número digitado. 8*/
 int main(){
-    unsigned short int n, acumuladora = 1;
+    unsigned short int n, acumuladora = 1, divisor;
     cout << "\nDigite um valor inteiro: ";
     cin >> n;
     cout << "\n" << n << " = " << n << "! = ";
@@ -13,5 +13,10 @@ int main(){
         if(i > 1) cout << " x ";
         acumuladora *= i;
     }
+    divisor = n+1;
+    while(divisor%n!=0){
+        divisor++;
+    }
     cout << " = " << acumuladora;
+    cout << "\nO proximo numero divisivel pelo valor digitado: " << divisor;
 }

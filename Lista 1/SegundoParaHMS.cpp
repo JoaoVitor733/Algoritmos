f65c2ed1f00b03dc5a77184cd84688cbe4d36ea2
João Vitor <83173020+joaovitor73@users.noreply.github.com>
2023-05-13 20:48:53 -0300

    Update and rename convertaSemHMS.cpp to SegundoParaHMS.cpp

--- Lista 1/SegundoParaHMS.cpp
+++ Lista 1/SegundoParaHMS.cpp
@@ -1,21 +1,14 @@
 #include <iostream>
 using namespace std;
-/*
-Leia o tempo de duração de um evento expresso em segundos, converta-o e mostre-o expresso 
-em horas, minutos e segundos. 
-    */
+/*Leia o tempo de duração de um evento expresso em segundos, converta-o e mostre-o expresso 
+em horas, minutos e segundos. */
 int main() {
     int s,h,m;
-
     cout << "Digite o tempo de duracao do evento: ";
     cin >> s; 
-
     h = s/3600; //horas
     s = s%3600;
     m = s/60;//segundos
     s = s%60;
-
     cout << h << "h "<< m << "m " << s << "s";
-     
-
 }

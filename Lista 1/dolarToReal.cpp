8ba6f49f2574131d7e75345f775be5b94e8043e6
João Vitor <83173020+joaovitor73@users.noreply.github.com>
2023-05-13 20:50:13 -0300

    Update dolarToReal.cpp

--- Lista 1/dolarToReal.cpp
+++ Lista 1/dolarToReal.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
 using namespace std;
-/*
-Escreva o valor em reais (R$) de um valor lido em dólares (US$). O algoritmo deverá solicitar o 
-valor da cotação do dólar e também a quantidade de dólares que o usuário deseja converter. 
-    */
+/*Escreva o valor em reais (R$) de um valor lido em dólares (US$). O algoritmo deverá solicitar o 
+valor da cotação do dólar e também a quantidade de dólares que o usuário deseja converter.  */
 int main() {
     int qtdD;
     float c;
-
     cout << "Digite a quantidade de dolar a ser convertido: ";
     cin >> qtdD;
-    cout << "\nDigite a cotacao do dolar: ";
+    cout << "\nDigite a cotacao do dolar em R$: ";
     cin >> c;
-
     cout << "\nValor em reais: R$" << c*qtdD; 
 }

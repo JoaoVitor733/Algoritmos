e5a69df91211d5881924e5c5fb5b5c113bd56fc7
João Vitor <83173020+joaovitor73@users.noreply.github.com>
2023-05-13 20:27:28 -0300

    Update celciusToFahr.cpp

--- Lista 1/celciusToFahr.cpp
+++ Lista 1/celciusToFahr.cpp
@@ -1,16 +1,12 @@
 #include <iostream>
 using namespace std;
-/*
-Leia uma temperatura em graus Celsius e apresente-a convertida em graus Fahrenheit. A 
+/* Leia uma temperatura em graus Celsius e apresente-a convertida em graus Fahrenheit. A 
 fórmula de conversão é F=(9*C+160)/5, sendo F a temperatura em Fahrenheit e C a temperatura 
-em Celsius.
-    */
+em Celsius. */
 int main() {
-    float c;
-
+    float C,F;
     cout << "\nDigite a temperatura(celsius): ";
-    cin >> c;
-
-    cout << "\nConvertendo para gruas Fahrenheit fica: "
-     << (9*c+160)/5;
+    cin >> C;
+    F = (9*C+160)/5;
+    cout << "\nConvertendo para gruas Fahrenheit fica: " << F;
 }

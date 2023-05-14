a5e3048055de12c7e382fbe113aec7607f95305f
João Vitor <83173020+joaovitor73@users.noreply.github.com>
2023-05-14 17:41:57 -0300

    Update ordemVetor.cpp

--- Lista 4/ordemVetor.cpp
+++ Lista 4/ordemVetor.cpp
@@ -2,8 +2,7 @@
 #include <iostream>
 using std::cin;
 using std::cout;
-/*
-Elabore um algoritmo que leia 20 elementos de um vetor de inteiros e, em seguida, um valor de código. Se o 
+/*Elabore um algoritmo que leia 20 elementos de um vetor de inteiros e, em seguida, um valor de código. Se o 
 código for 1, mostrar o vetor na ordem direta (do primeiro até o último), se o código for 2, mostrar o vetor 
 na ordem inversa (do último até o primeiro). */
 int main() {
@@ -13,7 +12,7 @@ int main() {
         cout << "\nDigite o valor da " << i+1 << "a. posicao: ";
         cin >> v[i];
     }
-    cout << "\nDigite a ordem que deseja vizualizar o vetor(1 - Direta e 0 - inversa: ";
+    cout << "\nDigite a ordem que deseja vizualizar o vetor(1 - Direta e 0 - inversa): ";
     cin >> ordem;
     if(ordem){
          for(int i=0; i<5;i++){

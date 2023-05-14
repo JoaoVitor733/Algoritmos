77a854b0aab53f039b93f44ff4aa9bef495371fa
João Vitor <83173020+joaovitor73@users.noreply.github.com>
2023-05-13 21:05:45 -0300

    Update operacoesEmBoolEfloat.cpp

--- Lista 1/operacoesEmBoolEfloat.cpp
+++ Lista 1/operacoesEmBoolEfloat.cpp
@@ -1,28 +1,22 @@
 #include <iostream>
 using namespace std;
-/*
-Leia dois valores inteiros para as variáveis A e B e efetue as operações de adição, subtração, 
+/*Leia dois valores inteiros para as variáveis A e B e efetue as operações de adição, subtração, 
 multiplicação e divisão de A por B, apresentando ao final os resultados obtidos. Em seguida leia 
 dois valores lógicos C e D e efetue as operações de negação (de cada um dos valores), conjunção 
-(E) e disjunção (OU), apresentando ao final os resultados obtidos.
-    */
+(E) e disjunção (OU), apresentando ao final os resultados obtidos.*/
 int main() {
-    float a, b;
-    
-    cout << "\nDigite o primeiro valor: ";
+    float a, b;   
+    cout << "\nDigite o 1o. valor: ";
     cin >> a;
     cout << "\nDigite o 2o. valor: ";
     cin >> b;
-
-    cout << "\nSoma: " << a+b << "\n\nSubtracao: " << a-b << "\n\nDivisao: " << a/b << "\n\nMultiplicacao: " << a*b;
-
+    cout << "\nSoma: " << a+b << "\n\nSubtracao: " << a-b  << "\n\nMultiplicacao: " << a*b;
+    if(b == 0) cout << "\n\nNao eh possivel dividir por 0";
+    else cout << "\n\nDivisao: " << a/b;
     bool c, d;
-
     cout << "\n\nDigite (0 ou 1) para C e para D respectivamente: ";
-   cin >> c >> d;
-
+    cin >> c >> d;
     cout << "\nNegação de C e D respectivamente: " << !c << " "<< !d;
     cout << "\n\nConjucao de C e D: " << (c && d);
     cout << "\n\nDisjucao de C e D: " << (c || d);
-
 }

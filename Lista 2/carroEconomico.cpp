70e29e8f3256de0422d195c68015302e33d28539
João Vitor <83173020+joaovitor73@users.noreply.github.com>
2023-05-13 21:31:24 -0300

    Update carroEconomico.cpp

--- Lista 2/carroEconomico.cpp
+++ Lista 2/carroEconomico.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
-/*
-Elabore um algoritmo que leia a distância em km e a quantidade de litros de gasolina consumidos por 
+/*Elabore um algoritmo que leia a distância em km e a quantidade de litros de gasolina consumidos por 
 um carro em um percurso, calcule o consumo em Km/l e escreva uma mensagem de acordo com a tabela 
 abaixo: 
 CONSUMO (Km/l) MENSAGEM 
@@ -10,14 +9,12 @@ Entre 8 e 12 Econômico!
 Maior que 12 Super econômico!*/
 int main(){
     float distancia, gasolina, consumo;
-
-    cout << "Digite os Km de um percurso: ";
+    cout << "\nDigite a distancia do percurso: ";
     cin >> distancia;
-    cout << "Digite o consumo de gasolina desse percurso: ";
+    cout << "\nDigite a quantidade gasta de gasolina: ";
     cin >> gasolina;
     consumo = distancia/gasolina;
-
-    if(consumo < 8) cout << "Venda o carro!!";
-    else if((consumo >= 8) && (consumo <= 12)) cout << "Economico!!";
+    if(consumo < 8) cout << "\nVenda o carro!!";
+    else if((consumo >= 8) && (consumo <= 12)) cout << "\nEconomico!!";
          else cout << "\nSuper economico!!";
 }

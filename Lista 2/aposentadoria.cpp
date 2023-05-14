8cf96ef1848a5c9731110e180528e3abaebc14d0
João Vitor <83173020+joaovitor73@users.noreply.github.com>
2023-05-13 21:19:40 -0300

    Update aposentadoria.cpp

--- Lista 2/aposentadoria.cpp
+++ Lista 2/aposentadoria.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
 using namespace std;
-/*
-Elabore um algoritmo que leia a idade e o tempo de serviço de um trabalhador e escreva se ele pode ou 
+/*Elabore um algoritmo que leia a idade e o tempo de serviço de um trabalhador e escreva se ele pode ou 
 não se aposentar. As condições para aposentadoria são: 
 • Ter pelo menos 65 anos;
 • Ou ter trabalhado pelo menos 30 anos; 
 • Ou ter pelo menos 60 anos e trabalhado pelo menos 25 anos.*/
 int main(){
-    int idade, tServico;
-
+    short int idade, tServico;
     cout << "\nDigite sua idade: ";
     cin >> idade;
     cout << "\nDigite seu tempo de servico: ";
     cin >> tServico;
-
-    if((idade >= 65) || (tServico >= 20) || (idade >= 60 && tServico >= 25))
+    if((idade >= 65) || (tServico >= 30) || (idade >= 60 && tServico >= 25))
         cout << "\nPode se aposentar!";
     else cout << "\nNao pode se aposentar!";
 }

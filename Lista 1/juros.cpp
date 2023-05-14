d44461273b7e2feb1c2fced11eadb8689acf91c0
João Vitor <83173020+joaovitor73@users.noreply.github.com>
2023-05-13 21:00:32 -0300

    Update juros.cpp

--- Lista 1/juros.cpp
+++ Lista 1/juros.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
 using namespace std;
-/*
-Leia um número inteiro N e um percentual qualquer e escreva qual o valor do percentual 
-aplicado ao número. 
-*/
+/*Leia um número inteiro N e um percentual qualquer e escreva qual o valor do percentual 
+aplicado ao número. */
 int main(){
     int n;
     float percentual;
-
     cout << "\nDigite um numero inteiro: ";
     cin >> n;
     cout << "\nDigite um percentual: ";
     cin >> percentual;
-
-    cout << "\nO juros eh: " << (percentual/100)*n;
+    cout << "\nO juros eh de: " << (percentual/100)*n;
 }

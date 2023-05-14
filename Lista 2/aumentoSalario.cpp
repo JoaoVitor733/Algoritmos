ba63d4efc24fb860bf54f8c97dbebd38e9faf988
João Vitor <83173020+joaovitor73@users.noreply.github.com>
2023-05-14 10:26:35 -0300

    Update aumentoSalario.cpp

--- Lista 2/aumentoSalario.cpp
+++ Lista 2/aumentoSalario.cpp
@@ -1,4 +1,4 @@
- #include <iostream>
+#include <iostream>
 using namespace std;
 /*Uma empresa concederá um aumento de salário aos seus funcionários, variável de acordo com o cargo, 
 conforme a tabela abaixo. Faça um algoritmo que leia o salário e o código do cargo de um funcionário e 
@@ -6,7 +6,7 @@ calcule o novo salário. Se o cargo do funcionário não estiver na tabela, ele 
 aumento. Mostre o salário antigo, o novo salário e a diferença. */
 int main(){
     int codigo;
-    float salario,novoSalario;
+    float salario,novoSalario; 
     cout << "\nDigite o seu salario: ";
     cin >> salario;
     cout << "\nDigite o codigo de seu cargo: ";

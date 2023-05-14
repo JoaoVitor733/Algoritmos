415ae800acda516129e0416f90391b187a4fa8aa
João Vitor <83173020+joaovitor73@users.noreply.github.com>
2023-05-14 10:14:12 -0300

    Update somaAlgarismo.cpp

--- Lista 2/somaAlgarismo.cpp
+++ Lista 2/somaAlgarismo.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 using namespace std;
-/*
- Elabore um algoritmo que leia um número inteiro maior do que zero (máximo de 5 algarismos), verifique 
+/*Elabore um algoritmo que leia um número inteiro maior do que zero (máximo de 5 algarismos), verifique 
 e escreva a soma de todos os seus algarismos. Por exemplo, para o número 251 a soma será 8 (2 + 5 + 
 1). Se o número lido não for maior do que zero, o programa terminar com a mensagem “Número 
-inválido”. 
-*/
+inválido”. */
 int main(){
     int numero, soma = 0, digito;
-
     cout << "\nDigite um valor interio maior que 0 e menor que 99999: ";
     cin >> numero;
     if(numero > 0){
@@ -22,9 +19,8 @@ int main(){
         soma += digito;
         digito = ((numero%10));
         soma += digito;
-        cout << "\nSomatorio dos digitos: " << soma;
+        cout << "\nA soma dos digitos eh: " << soma;
     }else{
         cout << "\nNumero invalido!";
     }
-
 }

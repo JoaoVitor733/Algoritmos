200e83c4adbb136b7e4ced763988562d5081714a
João Vitor <83173020+joaovitor73@users.noreply.github.com>
2023-05-13 21:13:49 -0300

    Update trocarValorVariaveis.cpp

--- Lista 1/trocarValorVariaveis.cpp
+++ Lista 1/trocarValorVariaveis.cpp
@@ -1,21 +1,16 @@
 #include <iostream>
 using namespace std;
-/*
-Leia dois valores inteiros nas variáveis Val1 e Val2, troque os seus conteúdos e escreva o 
+/*Leia dois valores inteiros nas variáveis Val1 e Val2, troque os seus conteúdos e escreva o 
 resultado. Exemplo: Se o valor lido foi 10 para a variável Val1 e 11 para a variável Val2, depois 
-da troca o algoritmo terá que escreva Val1 = 11 e Val2 = 10.
-    */
+da troca o algoritmo terá que escreva Val1 = 11 e Val2 = 10. */
 int main() {
     int v1,v2,aux;
-
     cout << "\nDigite o 1o. valor: ";
     cin >> v1;
     cout << "\nDigite o 2o. valor: ";
     cin >> v2;
-
     aux = v1;
     v1 = v2;
     v2 = aux;
-
     cout << "\nPrimeiro valor: " << v1 << " \n\nsegundo valor: " << v2;
 }

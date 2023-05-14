71aacc72b1d6012bae43fb398df6b921234740e8
João Vitor <83173020+joaovitor73@users.noreply.github.com>
2023-05-14 17:18:00 -0300

    Update elementosComuns.cpp

--- Lista 4/elementosComuns.cpp
+++ Lista 4/elementosComuns.cpp
@@ -6,7 +6,7 @@ um terceiro vetor C contendo todos os elementos comuns aos dois vetores A e B.*/
 int main() {
     int A[5], B[5], C[5];
     short int cont = 1, qtdComuns = 0;
-    bool flag = true, primeiraVez = true;
+    bool flag = true;
     for(int i=0; i < 5; i++){
         cout << "\n\nDigite o " << i+1<< "o. valor do vetor A: ";
         cin >> A[i];
@@ -17,10 +17,6 @@ int main() {
     for(int i=0; i < 5; i++){
         for(int j=0; j < 5; j++){
             if(A[i] == B[j]){
-                if(primeiraVez) {
-                    C[qtdComuns] = A[i];
-                    primeiraVez = false;
-                }
                 for(int k = 0; k < cont; k++){
                     if(A[i] == C[k]) flag = false;
                 }

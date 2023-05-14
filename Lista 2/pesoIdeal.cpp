76e6a2277ccd568b6c1906aa889e8de44d7edb00
João Vitor <83173020+joaovitor73@users.noreply.github.com>
2023-05-14 09:55:51 -0300

    Update pesoIdeal.cpp

--- Lista 2/pesoIdeal.cpp
+++ Lista 2/pesoIdeal.cpp
@@ -1,28 +1,18 @@
 
 #include <iostream>
 using namespace std;
-/*
-Calcule o peso ideal de uma pessoa. Dados de entrada: altura e gênero (“m”-masculino ou “f”-feminino). 
+/*Calcule o peso ideal de uma pessoa. Dados de entrada: altura e gênero (“m”-masculino ou “f”-feminino). 
 Utilize as seguintes fórmulas para cálculo do peso ideal: 
 • Masculino = (72,7 x altura) - 58 
-• Feminino = (62,1 x altura) - 44,7 
-*/
-
+• Feminino = (62,1 x altura) - 44,7 */
 int main(){
-    float a;  
-    char s;
-
-    cout << "\nDigite a sua altura: ";
-    cin >> a;
+    float altura;  
+    char sexo;
+    cout << "\nDigite a sua altura(em metros): ";
+    cin >> altura;
     cout << "\nDigite seu sexo('m'-masculino ou 'f' feminino: ";
-    cin >> s;
-
-    if(s == 'm'){
-        cout << "\nSeu peso ideal: " << (72.7*a)-58;
-    }else if(s == 'f'){
-        cout << "\nSeu peso ideal: " << (62.1*a) - 44.7; 
-    }else{
-        cout << "letra invalida!!";
-    }
-    
+    cin >> sexo;
+    if(sexo == 'm') cout << "\nSeu peso ideal: " << (72.7*altura)-58;
+    else if(sexo == 'f') cout << "\nSeu peso ideal: " << (62.1*altura) - 44.7; 
+    else cout << "letra invalida!!";    
 }

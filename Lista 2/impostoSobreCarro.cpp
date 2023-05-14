f3d5a023c468229f99b515994f449b149a16b4ca
João Vitor <83173020+joaovitor73@users.noreply.github.com>
2023-05-14 09:32:00 -0300

    Update impostoSobreCarro.cpp

--- Lista 2/impostoSobreCarro.cpp
+++ Lista 2/impostoSobreCarro.cpp
@@ -1,22 +1,17 @@
 
 #include <iostream>
 using namespace std;
-/*
-Num determinado Estado, para transferências de veículos o DETRAN cobra uma taxa de 1% para carros 
+/*Num determinado Estado, para transferências de veículos o DETRAN cobra uma taxa de 1% para carros 
 fabricados antes de 1990 e uma taxa de 1.5% para os fabricados a partir de 1990, taxa esta que incide 
 sobre o preço do carro. Elabore um algoritmo que leia o ano e o preço do carro, calcule e escreva o 
-imposto a ser pago. 
-*/
-
+imposto a ser pago. */
 int main(){
-    int a;
-    float p;
-
-    cout << "\nDigite o ano de fabricacao de seu carro: ";
-    cin >> a;
-    cout << "\nDigite o preco de seu carro:";
-    cin >> p;
-
-    if(a < 1990) cout << "\nImposto a ser pago: " << p*0.01;     
-    else  cout << "\nImposto a ser pago: " << p*0.15; 
-}
+    int ano;  
+    float preco;
+    cout << "\nDigite o ano de fabricacao do seu carro: ";
+    cin >> ano;
+    cout << "\nDigite o preco do seu carro:";
+    cin >> preco;
+    if(ano < 1990) cout << "\nImposto a ser pago: " << preco*0.01;     
+    else  cout << "\nImposto a ser pago: " << preco*0.15; 
+}    

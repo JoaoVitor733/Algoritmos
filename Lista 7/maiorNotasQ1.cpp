a77ff87264aadc9452482f8c8e545ef341cf676e
João Vitor <83173020+joaovitor73@users.noreply.github.com>
2023-06-27 12:27:45 -0300

    Update maiorNotasQ1.cpp

--- Lista 7/maiorNotasQ1.cpp
+++ Lista 7/maiorNotasQ1.cpp
@@ -7,22 +7,9 @@ algoritmo que, dadas as quatro notas de um aluno, informe a sua média. Para iss
 duas funções: uma que determine o maior valor entre as notas (de provas ou de 
 trabalhos) e outra que calcule a média aritmética das duas maiores notas. */
 
-float calcularMedia(float maiores[]){
-    return (maiores[0]+maiores[1])/2;
-}
+float calcularMedia(float maiores[]);
 
-float maioresNotas(float notas[]){
-    short int cont = 0;
-    float media, maiores[2];
-    for(int i = 1; i < 4; i+=2){
-       if(notas[i] < notas[i-1]) notas[i] = notas[i-1];
-       maiores[cont] = notas[i];
-       cont++;
-    }
-
-    media = calcularMedia(maiores);
-    return media;
-}
+float maioresNotas(float notas[]);
 
 int main(){
     float notas[4];
@@ -43,4 +30,21 @@ int main(){
     }
     media = maioresNotas(notas);
     cout << "\nMedia final: " << media;
+}
+
+float calcularMedia(float maiores[]){
+    return (maiores[0]+maiores[1])/2;
+}
+
+float maioresNotas(float notas[]){
+    short int cont = 0;
+    float media, maiores[2];
+    for(int i = 1; i < 4; i+=2){
+       if(notas[i] < notas[i-1]) notas[i] = notas[i-1];
+       maiores[cont] = notas[i];
+       cont++;
+    }
+
+    media = calcularMedia(maiores);
+    return media;
 }
